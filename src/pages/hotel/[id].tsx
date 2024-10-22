e009d6d58f897355cc1692e7b705eadee3732e52
BrounouSalah <abdelkhalek.salah01@gmail.com>
2024-10-22 18:40:24 +0100

    add client request creation from hotel page

--- src/pages/hotel/[id].tsx
+++ src/pages/hotel/[id].tsx
@@ -14,6 +14,23 @@ import {
   Container,
   useColorModeValue,
   useBreakpointValue,
+  Modal,
+  ModalOverlay,
+  ModalHeader,
+  ModalContent,
+  ModalCloseButton,
+  ModalBody,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  ModalFooter,
+  useDisclosure,
+  CheckboxGroup,
+  Checkbox,
+  FormErrorMessage,
+  Select,
+  useToast,
 } from "@chakra-ui/react";
 import {
   FaFacebook,
@@ -35,6 +52,96 @@ import { Link } from "react-scroll";
 const HotelPreviewPage = ({ hotelData, servicesData }) => {
   const [hotel, setHotel] = useState(hotelData);
   const [services, setServices] = useState(servicesData);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    date_of_birth: "",
+    nationality: "",
+    gender: "Male",
+    phone_number: "",
+    check_in: "",
+    check_out: "",
+    notes: "",
+    adult_number: 1,
+    children_number: 0,
+    selectedServices: [] as any[],
+    isSubmitting: false,
+  });
+  const [errors, setErrors] = useState<any>({});
+  const toast = useToast();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleServiceSelection = (values) => {
+    setFormData((prev) => ({ ...prev, selectedServices: values }));
+  };
+
+  const handleSubmit = async () => {
+    if (!validateForm()) return;
+    setFormData((prev) => ({ ...prev, isSubmitting: true }));
+    const payload = {
+      hotel_id: hotel.id,
+      referent: {
+        first_name: formData.first_name,
+        last_name: formData.last_name,
+        email: formData.email,
+        date_of_birth: formData.date_of_birth,
+        phone_number: formData.phone_number,
+        person_type: "Adult",
+        nationality: formData.nationality,
+        gender: formData.gender,
+      },
+      check_in: new Date(formData.check_in).toISOString(),
+      check_out: new Date(formData.check_out).toISOString(),
+      services: formData.selectedServices,
+      client_request_status: "Pending",
+      notes: formData.notes,
+      adult_number: +formData.adult_number,
+      children_number: +formData.children_number,
+    };
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API}/client-requests`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to submit the request.");
+      }
+      toast({
+        title: "Demande envoyée.",
+        description: "Votre demande a été envoyée avec succès.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+
+      onClose();
+    } catch (error) {
+      console.error("Error submitting the request:", error);
+      toast({
+        title: "Erreur.",
+        description: "Une erreur est survenue lors de l'envoi de la demande.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    } finally {
+      setFormData((prev) => ({ ...prev, isSubmitting: false }));
+    }
+  };
 
   const isMobile = useBreakpointValue({ base: true, md: false });
 
@@ -51,6 +158,25 @@ const HotelPreviewPage = ({ hotelData, servicesData }) => {
     return french_days[day];
   };
 
+  const handleOpenModal = () => {
+    onOpen();
+  };
+
+  const validateForm = () => {
+    const newErrors: any = {};
+    if (!formData.first_name) newErrors.first_name = "Prénom requis";
+    if (!formData.last_name) newErrors.last_name = "Nom requis";
+    if (!formData.email) newErrors.email = "Email requis";
+    if (!formData.gender) newErrors.gender = "Genre requis";
+    if (!formData.nationality) newErrors.nationality = "Nationnalité requise";
+    if (!formData.date_of_birth)
+      newErrors.date_of_birth = "Date de naissance requise";
+    if (!formData.phone_number) newErrors.phone_number = "Téléphone requis";
+    if (!formData.check_in) newErrors.check_in = "Check-in requis";
+    if (!formData.check_out) newErrors.check_out = "Check-out requis";
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
   return (
     <Box>
       {/* Navbar */}
@@ -81,7 +207,7 @@ const HotelPreviewPage = ({ hotelData, servicesData }) => {
           )}
         </HStack>
 
-        <Button colorScheme="blue" size="md">
+        <Button onClick={() => handleOpenModal()} colorScheme="blue" size="md">
           Réserver maintenant
         </Button>
       </Flex>
@@ -114,7 +240,12 @@ const HotelPreviewPage = ({ hotelData, servicesData }) => {
             <Text textAlign={isMobile ? "center" : "start"} fontSize="lg">
               Découvrez notre hôtel de luxe et réservez votre séjour maintenant.
             </Text>
-            <Button colorScheme="blue" size="lg" mb={2}>
+            <Button
+              onClick={() => handleOpenModal()}
+              colorScheme="blue"
+              size="lg"
+              mb={2}
+            >
               Réserver maintenant
             </Button>
           </VStack>
@@ -217,7 +348,11 @@ const HotelPreviewPage = ({ hotelData, servicesData }) => {
           <Heading as="h2" size="xl" mb={4}>
             Réservez votre séjour maintenant
           </Heading>
-          <Button colorScheme="blue" size="lg">
+          <Button
+            onClick={() => handleOpenModal()}
+            colorScheme="blue"
+            size="lg"
+          >
             Réserver maintenant
           </Button>
         </Container>
@@ -356,6 +491,158 @@ const HotelPreviewPage = ({ hotelData, servicesData }) => {
           </VStack>
         </Container>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose} size={"xl"}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Demande de Réservation</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody overflowY="auto" maxHeight="calc(90vh - 200px)">
+            <FormControl isInvalid={errors.first_name} isRequired>
+              <FormLabel>Prénom</FormLabel>
+              <Input
+                name="first_name"
+                value={formData.first_name}
+                onChange={handleChange}
+              />
+              <FormErrorMessage>{errors.first_name}</FormErrorMessage>
+            </FormControl>
+            <FormControl mt={4} isInvalid={errors.last_name} isRequired>
+              <FormLabel>Nom</FormLabel>
+              <Input
+                name="last_name"
+                value={formData.last_name}
+                onChange={handleChange}
+              />
+              <FormErrorMessage>{errors.last_name}</FormErrorMessage>
+            </FormControl>
+            <FormControl mt={4} isInvalid={errors.email} isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              <FormErrorMessage>{errors.email}</FormErrorMessage>
+            </FormControl>
+            <FormControl mt={4} isInvalid={errors.date_of_birth} isRequired>
+              <FormLabel>Date de naissance</FormLabel>
+              <Input
+                type="date"
+                name="date_of_birth"
+                value={formData.date_of_birth}
+                onChange={handleChange}
+              />
+              <FormErrorMessage>{errors.date_of_birth}</FormErrorMessage>
+            </FormControl>
+            <FormControl mt={4} isInvalid={errors.phone_number} isRequired>
+              <FormLabel>Téléphone</FormLabel>
+              <Input
+                name="phone_number"
+                value={formData.phone_number}
+                onChange={handleChange}
+              />
+              <FormErrorMessage>{errors.phone_number}</FormErrorMessage>
+            </FormControl>
+            <FormControl mt={4} isInvalid={errors.nationality} isRequired>
+              <FormLabel>Nationnalité</FormLabel>
+              <Input
+                name="nationality"
+                value={formData.nationality}
+                onChange={handleChange}
+              />
+              <FormErrorMessage>{errors.nationality}</FormErrorMessage>
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Type de personne</FormLabel>
+              <Select
+                name="gender"
+                value={formData.gender}
+                onChange={(e) => handleChange(e)}
+              >
+                <option value="Male">Homme</option>
+                <option value="Female">Femme</option>
+              </Select>
+            </FormControl>
+            <FormErrorMessage>{errors.gender}</FormErrorMessage>
+            <FormControl mt={4} isInvalid={errors.check_in} isRequired>
+              <FormLabel>Check-in</FormLabel>
+              <Input
+                type="date"
+                name="check_in"
+                value={formData.check_in}
+                onChange={handleChange}
+              />
+              <FormErrorMessage>{errors.check_in}</FormErrorMessage>
+            </FormControl>
+            <FormControl mt={4} isInvalid={errors.check_out} isRequired>
+              <FormLabel>Check-out</FormLabel>
+              <Input
+                type="date"
+                name="check_out"
+                value={formData.check_out}
+                onChange={handleChange}
+              />
+              <FormErrorMessage>{errors.check_out}</FormErrorMessage>
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Services</FormLabel>
+              <CheckboxGroup
+                colorScheme="blue"
+                value={formData.selectedServices}
+                onChange={handleServiceSelection}
+              >
+                <VStack align="start">
+                  {services.map((service) => (
+                    <Checkbox key={service.id} value={service.id}>
+                      {service.service_name}
+                    </Checkbox>
+                  ))}
+                </VStack>
+              </CheckboxGroup>
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Nombre d'adultes</FormLabel>
+              <Input
+                type="number"
+                name="adult_number"
+                value={formData.adult_number}
+                onChange={handleChange}
+                min={1}
+              />
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Nombre d'enfants</FormLabel>
+              <Input
+                type="number"
+                name="children_number"
+                value={formData.children_number}
+                onChange={handleChange}
+                min={0}
+              />
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Notes</FormLabel>
+              <Textarea
+                name="notes"
+                value={formData.notes}
+                onChange={handleChange}
+              />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={handleSubmit}
+              isLoading={formData.isSubmitting}
+              disabled={formData.isSubmitting}
+            >
+              Envoyer la demande
+            </Button>
+            <Button onClick={onClose}>Annuler</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
